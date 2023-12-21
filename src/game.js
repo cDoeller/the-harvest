317a8ab39351a733bf0afaed5fb42ea2dbaaf45a
Christian Doeller <mail@christiandoeller.de>
2023-12-21 19:53:05 +0100

    made code simpler by adding ID variables to target class

--- src/game.js
+++ src/game.js
@@ -9,7 +9,7 @@ class Game {
     this.targetID = 0;
     this.targetAmount = 10;
     this.scaleFacotrs = [5, 15, 25]; // possible scale factors
-    this.maxSpeed = 1.2;
+    this.maxSpeed = 2.5;
     this.minSpeed = 0.5;
     this.spawnTargetBottom = 100; // spawning always at same y height
 
@@ -19,49 +19,57 @@ class Game {
 
     // scores for different sizes + penalty
     this.score = 0;
-    this.penaltyVanish = 5;
-    this.scoreSmall = 15;
-    this.scoreMedium = 10;
-    this.scoreLarge = 5;
+    this.balloonsHit = 0;
+    this.broccolisHit = 0;
+    this.clickCount = 0;
 
     // global ID for interval
-    this.intervalID;
+    this.mainIntervalID;
   }
 
+  // ** START FUNCTION
   start() {
     console.log("game.js start function");
     // hide the start screen, show game screen
     this.startScreen.style.display = "none";
     this.gameScreen.style.display = "block";
 
-    // display time in very beginning
+    // display time in first moments of game
     this.displayTime();
 
     // spawn initial targets
     this.spawnTargtes(this.targetAmount);
 
     // start interval for countdown
-    this.intervalID = setInterval(() => {
+    this.mainIntervalID = setInterval(() => {
       this.countdown--;
       this.displayTime();
       //console.log(this.countdown);
     }, 1000);
 
+    // add listener for entire window clicks
+    window.addEventListener("mousedown", () => {
+      this.clickCount++;
+      console.log(this.clickCount);
+    });
+
     // Start the game loop
     this.gameLoop();
   }
 
+  // ** GAME LOOP FUNCTION
   gameLoop() {
     // stop when countdown === 0
     if (this.countdown <= 0) {
       console.log("time is up");
-      console.log(this.targets);
+      //console.log(this.targets);
       this.gameIsOver = true;
-      clearInterval(this.intervalID);
+      clearInterval(this.mainIntervalID);
     }
 
     // Interrupt the function to stop the loop if "gameIsOver" is set to "true"
     if (this.gameIsOver) {
+      this.displayStats();
       //call endGame();
       return;
     }
@@ -69,61 +77,61 @@ class Game {
     // update what can be seen on the game canvas
     this.updateGame();
 
-    // 60 fps interval, continuesly calling itself
+    // 60 fps interval, continuesly calling itself (gameLoop function)
     window.requestAnimationFrame(() => this.gameLoop());
   }
 
+  // ** UPDATE FUNCTION
   updateGame() {
     // loop through all targets
-    // 1. move up
-    // 2. display
-    // 3. erase
     this.targets.forEach((target, index) => {
-      // target gone
+      // ** TARGET GONE
       if (target.isGone() === true) {
         // penalty points
-        this.score -= this.penaltyVanish;
-        console.log(`removed ${this.score}`);
+        this.score -= target.score;
         // delete from array
         this.targets.splice(index, 1);
         // make a replacement
         this.spawnTargtes(1);
-        // no further displaying for this target --> escape
+        // --> escape to not do the other steps
         return;
       }
-      // target hit (balloon)
+      // ** TARGET HIT (balloon)
       if (target.balloonIsHit === true) {
         // score points
-        if (target.scaleFactor === this.scaleFacotrs[0])
-          this.score += this.scoreSmall;
-        if (target.scaleFactor === this.scaleFacotrs[1])
-          this.score += this.scoreMedium;
-        if (target.scaleFactor === this.scaleFacotrs[2])
-          this.score += this.scoreLarge;
-        console.log(`balloon hit ${this.score}`);
+        this.score += target.score;
+        // increase targets hit
+        this.balloonsHit++;
+        // reset the hit flag of balloon
         target.balloonIsHit = false;
         // make new target
         this.spawnTargtes(1);
       }
-      // target hit (broccoli)
+      // ** TARGET HIT (broccoli)
       if (target.broccoliIsHit === true) {
         // penalty points
-        this.score -= this.penaltyVanish;
-        console.log(`broccoli hit ${this.score}`);
+        this.score -= target.score;
+        // increase broccoli hit
+        this.broccolisHit ++;
+        // reset the hit flag of broccoli
         target.broccoliIsHit = false;
       }
       // MOVE or GROW
-      if (target.positionY < this.spawnTargetBottom && target.targetRising === false) {
-        // change to growing image
-        target.growInGround(target.targetNumber);
+      // if target falling down and below spawn line
+      if (
+        target.positionY < this.spawnTargetBottom &&
+        target.targetRising === false
+      ) {
+        // change to growing image on spawn line
+        target.growInGround();
       } else {
-        // move up or down
+        // if still moving -> rise up or fall down
         target.moveTarget();
       }
       // display
       target.displayTarget();
     });
-
+    // display score after updates
     this.displayScore();
   }
 
@@ -162,11 +170,22 @@ class Game {
     timeH1.innerHTML = `${timeString}`;
   }
 
+  // display all the stats
+  displayStats() {
+    const missed = this.clickCount - this.balloonsHit - this.broccolisHit;
+    console.log(`your stats: \n 
+    darts thrown: ${this.clickCount} \n
+    targets hit: ${this.balloonsHit}\n
+    broccolis hit: ${this.broccolisHit}\n
+    missed darts: ${missed}`);
+  }
+
   spawnTargtes(amount) {
     // constructor(
     //   spawnPositionX,
     //   spawnPositionY,
     //   scaleFactor, ------> [0.5, 1, 1.5]
+    //   score,
     //   speed,
     //   targetNumber
     // )
@@ -174,21 +193,23 @@ class Game {
       // define random Properties of targets
       // get random scale factor (size of target)
       const randScaleFactorIndex = Math.floor(Math.random() * 3);
-      const currentScaleFacotr = this.scaleFacotrs[randScaleFactorIndex];
-      console.log(`current factor: ${currentScaleFacotr}`);
+      const currentScaleFactor = this.scaleFacotrs[randScaleFactorIndex];
+      // assign the score
+      const currentScore = currentScaleFactor;
       // get a random spawn position only in window width, at certain Y height above ground
       const randSpawnPosX =
-        Math.random() * (window.innerWidth - currentScaleFacotr * 4);
+        Math.random() * (window.innerWidth - currentScaleFactor * 4);
       // get random speed: Math.random() * (max - min) + min;
       const randSpeed =
         Math.random() * (this.maxSpeed - this.minSpeed) + this.minSpeed;
-      console.log(`speed: ${randSpeed}`);
+      //console.log(`speed: ${randSpeed}`);
 
       // make the targets and put in array
       const tempTarget = new Target(
         randSpawnPosX,
         this.spawnTargetBottom,
-        currentScaleFacotr,
+        currentScaleFactor,
+        currentScore,
         randSpeed,
         this.targetID
       );
--- src/targets.js
+++ src/targets.js
@@ -3,35 +3,50 @@ class Target {
     spawnPositionX,
     spawnPositionY,
     scaleFactor,
+    score,
     speed,
     targetNumber
   ) {
+
+    // assign variables to object
     this.spawnPositionX = spawnPositionX;
     this.spawnPositionY = spawnPositionY;
     this.scaleFactor = scaleFactor;
+    this.score = score;
     this.speed = speed;
-    this.fallingSpeed = 3;
-    this.positionX = spawnPositionX; // initial pos at spawn pos
-    this.positionY = spawnPositionY; // initial pos at spawn pos
     this.targetNumber = targetNumber;
+
+    // initial position when spawning
+    this.positionX = spawnPositionX;
+    this.positionY = spawnPositionY;
+
+    // save the entire ID 
+    this.targetID = "";
+    this.broccoliID = "";
+    this.balloonID = "";
+
+    // speed for falling down
+    this.fallingSpeed = 5; 
+
     this.balloonIsHit = false;
     this.broccoliIsHit = false;
     this.broccoliWasHit = false;
     this.targetRising = true;
     this.growing = false;
+
     // get game screen for appending targets
     this.gameScreen = document.getElementById("game-screen");
   }
 
   makeTarget() {
-    console.log("targets.js makeTarget");
-    console.log(`${this.positionX}`, `${this.positionY}`);
+    // console.log("targets.js makeTarget");
+    // console.log(`${this.positionX}`, `${this.positionY}`);
 
     // make a target container element
     const targetContainer = document.createElement("div");
     targetContainer.className = "target-container";
-    targetContainer.id = `target-${this.targetNumber}`;
-    // target counter needed for exact id for positioning?
+    this.targetID = `target-${this.targetNumber}`;
+    targetContainer.id = this.targetID;
     // position target initially
     targetContainer.style.position = "absolute";
     targetContainer.style.left = `${this.positionX}px`;
@@ -41,44 +56,45 @@ class Target {
     const targetHeight = 9 * this.scaleFactor;
     targetContainer.style.width = `${targetWidth}px`;
     targetContainer.style.height = `${targetHeight}px`;
-    targetContainer.style.backgroundColor = "none";
-    targetContainer.style.display = "none";
+    //targetContainer.style.display = "none";
     // append target
     this.gameScreen.appendChild(targetContainer);
 
     // make a balloon element
     const balloon = document.createElement("div");
     balloon.className = "balloon-class";
-    balloon.id = `balloon-${this.targetNumber}`;
+    this.balloonID = `balloon-${this.targetNumber}`;
+    balloon.id = this.balloonID;
+    // style balloon element
     balloon.style.position = "absolute";
     balloon.style.left = "0";
     balloon.style.top = "0";
     balloon.style.width = "100%";
     balloon.style.height = `${(5 / 9) * 100}%`;
-    balloon.style.backgroundColor = "none";
     // append balloon
     targetContainer.appendChild(balloon);
 
     // make a broccoli element
     const broccoli = document.createElement("div");
     broccoli.className = "broccoli-class";
-    broccoli.id = `broccoli-${this.targetNumber}`;
+    this.broccoliID = `broccoli-${this.targetNumber}`;
+    broccoli.id = this.broccoliID;
+    // style broccoli
     broccoli.style.position = "absolute";
     broccoli.style.left = "0";
     broccoli.style.top = `${(5 / 9) * 100}%`;
     broccoli.style.width = "100%";
     broccoli.style.height = `${(4 / 9) * 100}%`;
-    broccoli.style.backgroundColor = "none";
     // append balloon
     targetContainer.appendChild(broccoli);
 
     // add event listeners
     // pass specific id of elements
     balloon.addEventListener("mousedown", () => {
-      this.ballooniHit(targetContainer.id, balloon.id, broccoli.id);
+      this.ballooniHit();
     });
     broccoli.addEventListener("mousedown", () => {
-      this.broccoliHit(broccoli.id);
+      this.broccoliHit();
     });
   }
 
@@ -106,14 +122,14 @@ class Target {
     }
   }
 
-  ballooniHit(targetId, balloonId, broccoliId) {
+  ballooniHit() {
     // change state
     this.balloonIsHit = true;
     this.targetRising = false;
     // add explosion sprite
-    const balloon = document.getElementById(`${balloonId}`);
-    const target = document.getElementById(`${targetId}`);
-    const broccoli = document.getElementById(`${broccoliId}`);
+    const balloon = document.getElementById(this.balloonID);
+    const target = document.getElementById(`${this.targetID}`);
+    const broccoli = document.getElementById(`${this.broccoliID}`);
     // put smallest in front
     if (this.scaleFactor == 5) broccoli.style.zIndex = "3";
     if (this.scaleFactor == 10) broccoli.style.zIndex = "2";
@@ -133,31 +149,22 @@ class Target {
     }, 50);
   }
 
-  broccoliHit(id) {
+  broccoliHit() {
     // change state
     this.broccoliIsHit = true;
     this.broccoliWasHit = true;
     // add dart sprite
-    const broccoli = document.getElementById(`${id}`);
+    const broccoli = document.getElementById(`${this.broccoliID}`);
     broccoli.classList.add("broccoli-hit");
-    // setTimeout(() => {
-    //   broccoli.classList.remove("broccoli-hit");
-    // }, 500);
   }
 
-  growInGround(targetNum) {
-    // if (this.growing === false) {
-    //   for (let i = 0; i < Math.random() * 50; i++) {
-    //     this.moveTarget();
-    //   }
-    // }
-    const broccoli = document.getElementById(`broccoli-${targetNum}`);
+  growInGround() {
+    const broccoli = document.getElementById(`${this.broccoliID}`);
     // change growing image (if prev hit, grow rotten)
     if (!this.broccoliWasHit) {
       broccoli.classList.add("broccoli-growing");
     } else {
       broccoli.classList.add("broccoli-growing-hit");
     }
-    // this.growing = true;
   }
 }
